048262a3dcc9aa6ae749f3153b5736daf615d87b
wujianfeng <wujianfeng@yaxon.com>
2020-02-24 15:03:16 +0800

    Initial commit

---
+++ app/src/main/java/com/oneday/videodemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oneday.videodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
